83a36ea6d7cb2b090d8f6bfad01dffd8fee75713
da593 <dylanau99@gmail.com>
2021-09-16 21:50:52 -0400

    rename files to prevent import errors

---
+++ project/marketplaceapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.getHomePage, name='home'),
+    path('home', views.getHomePage, name='home'),
+    path('pearlmarket', views.getPearlMarketPage, name='pearlmarket'),
+    path('cooking', views.getCookingPage, name='cooking'),
+    path('alchemy', views.getAlchemyPage, name='alchemy'),
+    path('farming', views.getFarmingPage, name='farming'),
+]
