1702aac63395633ba4248e27eeb77a5ec84b26a0
da593 <da593@DESKTOP-Q6B6H1A.localdomain>
2021-12-04 18:26:16 -0500

    Added environment variables and change config settings

---
+++ frontend/src/pages/App.js
@@ -0,0 +1,11 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/PageProperties.js
@@ -0,0 +1,37 @@
+export const PageNames = [
+    {
+        title:"Home",
+        url:'BDOTracker/',
+    },
+    {
+        title:"Home",
+        url:'BDOTracker/Home',
+    },
+    
+    {
+        title:"PearlMarket",
+        url:'BDOTracker/pearlmarket',
+        
+    },
+    {
+        title:"PearlMarket",
+        url:'BDOTracker/pearlmarket',
+        
+    },
+    {
+        title:"Cooking",
+        url:'BDOTracker/cooking',
+        
+    },
+    {
+        title:"Alchemy",
+        url:'BDOTracker/alchemy',
+        
+    },
+    {
+        title:"Farming",
+        url:'BDOTracker/farming',
+    },
+
+
+]
